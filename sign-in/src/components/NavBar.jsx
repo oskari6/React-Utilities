bcab01974a4aa9312c84e7030f56f23a8487c303
oskari6 <oskarisulkakoski@gmail.com>
2025-02-08 19:51:57 +0200

    some animation function

---
+++ sign-in/src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import SidePanel from "../animations/SidePanel";
+import { useState } from "react";
+import Spinner from "../animations/Spinner";
+
+export default function NavBar() {
+  const [isPanelOpen, setIsPanelOpen] = useState(false);
+
+  return (
+    <div>
+      <button
+        className="flex items-center justify-center bg-gray-100"
+        onClick={() => setIsPanelOpen(true)}
+      >
+        Open Menu
+      </button>
+
+      <SidePanel isOpen={isPanelOpen} onClose={() => setIsPanelOpen(false)}>
+        <h2 className="text-xl font-bold">Menu</h2>
+        <Spinner />
+        <ul className="mt-4">
+          <li className="py-2">
+            <nav>
+              <Link to="/">Home</Link> |
+            </nav>
+          </li>
+          <li className="py-2">
+            <nav>
+              <Link to="/animations">Animations</Link>
+            </nav>
+          </li>
+          <li className="py-2">
+            <nav>
+              <Link to="/animations2">Animations 2</Link>
+            </nav>
+          </li>
+        </ul>
+      </SidePanel>
+    </div>
+  );
+}
